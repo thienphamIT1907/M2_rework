f35d614af5525a9c05c578616932863e8452cb4b
Thien Pham <phamanhthien1907@gmail.com>
2020-05-31 15:47:58 +0700

    Hoan thanh bai thuc hanh: Ung dung login

---
+++ Bai5_SpringController/ThucHanh/Bai1_UngDungLogin/src/controllers/UserController.java
@@ -0,0 +1,31 @@
+package controllers;
+
+import DAO.UserDao;
+import models.Login;
+import models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/home")
+    public ModelAndView home(){
+        return new ModelAndView("home", "login", new Login());
+    }
+
+    @PostMapping("/login")
+    public ModelAndView login(@ModelAttribute("login") Login login){
+        User user = UserDao.checkLogin(login);
+        if(user == null){
+            return new ModelAndView("error");
+        } else {
+            ModelAndView modelAndView = new ModelAndView("user");
+            modelAndView.addObject("user", user);
+            return modelAndView;
+        }
+    }
+}
