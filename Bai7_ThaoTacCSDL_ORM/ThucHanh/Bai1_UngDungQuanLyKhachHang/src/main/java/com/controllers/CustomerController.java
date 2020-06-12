cace1a4267c7761df28baf1ea4da57c4626e5aac
Thien Pham <phamanhthien1907@gmail.com>
2020-06-12 09:05:00 +0700

    Hoan thanh bai thuc hanh: Ung dung quan ly khach hang

---
+++ Bai7_ThaoTacCSDL_ORM/ThucHanh/Bai1_UngDungQuanLyKhachHang/src/main/java/com/controllers/CustomerController.java
@@ -0,0 +1,53 @@
+package com.controllers;
+
+import com.models.Customer;
+import com.services.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class CustomerController {
+
+    @Autowired
+    CustomerService customerService;
+
+    @GetMapping("/")
+    public ModelAndView showList() {
+        List<Customer> customers = customerService.findAll();
+        ModelAndView modelAndView = new ModelAndView("/customer/list");
+        modelAndView.addObject("customers", customers);
+        return modelAndView;
+    }
+
+    @GetMapping("/create-customer")
+    public ModelAndView showCreateForm(){
+        ModelAndView modelAndView = new ModelAndView("/customer/create");
+        modelAndView.addObject("customer", new Customer());
+        return modelAndView;
+    }
+
+    @PostMapping("/create-customer")
+    public String saveCustomer(@ModelAttribute("customer") Customer customer){
+        customerService.save(customer);
+        return "redirect:/";
+    }
+
+    @GetMapping("/customers")
+    public ModelAndView listCustomers(){
+        List<Customer> customers = customerService.findAll();
+        ModelAndView modelAndView = new ModelAndView("/customer/list");
+        modelAndView.addObject("customers", customers);
+        return modelAndView;
+    }
+
+    @GetMapping(value = {"/edit/{id}", "/delete/{id}"})
+    public String redirectMaintaince() {
+        return "/customer/maintaince";
+    }
+}
