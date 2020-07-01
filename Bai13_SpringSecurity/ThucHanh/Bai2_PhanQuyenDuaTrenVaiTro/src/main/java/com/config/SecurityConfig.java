8b8694a8318117f9543e137dc2b92794ec76d1f4
Thien Pham <phamanhthien1907@gmail.com>
2020-07-01 08:52:48 +0700

    Hoan thanh bai thuc hanh: Phan quyen dua tren vai tro

---
+++ Bai13_SpringSecurity/ThucHanh/Bai2_PhanQuyenDuaTrenVaiTro/src/main/java/com/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    CustomSuccessHandler customSuccessHandler;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("bill").password(passwordEncoder().encode("12345")).roles("USER");
+        auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder().encode("12345")).roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("dba").password(passwordEncoder().encode("12345")).roles("ADMIN", "DBA");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "/home").access("hasRole('USER')")
+                .antMatchers("/admin/**").access("hasRole('ADMIN')")
+                .antMatchers("/dba/**").access("hasRole('ADMIN') and hasRole('DBA')")
+                .and().formLogin().successHandler(customSuccessHandler)
+                .usernameParameter("ssoId").passwordParameter("password")
+                .and().csrf()
+                .and().exceptionHandling().accessDeniedPage("/Access_Denied");
+    }
+}
